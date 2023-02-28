f810d4876ea76cd984b5f1784b396040985bfa4b
Benik <galstyan.benik@gmail.com>
2023-02-28 12:48:47 +0200

    Fetching data from Api

--- lib/listview.dart
+++ lib/listview.dart
@@ -1,52 +1,68 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mocked_list_of_breeds/models/breed.dart';
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  final List<String> breeds = [
-    "affenpincher",
-    "african",
-    "airedale ",
-    "akita",
-    "appenzeller ",
-    "australian shepherd",
-    "basenji",
-    "beagle  ",
-    "bluetick ",
-    "borzoi ",
-    "bouvier",
-    "boxer",
-    "brabancon",
-  ];
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Breed> _breeds = [];
+
+  Future<void> getData() async {
+    final response =
+        await http.get(Uri.parse('https://dog.ceo/api/breeds/list/all'));
+    var responseMapBreeds = json.decode(response.body);
+    var breedsMap = responseMapBreeds['message'];
+
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
   final nameOfScreen = "Dogs List Screen";
+
   final double heightOption = 60;
+
   final double thicknessOption = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(nameOfScreen),
-          backgroundColor: Colors.black,
-        ),
-        body: ListView.separated(
-          physics: const BouncingScrollPhysics(),
-          itemCount: breeds.length,
-          itemBuilder: (_, index) => Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Text(
-              breeds[index],
-              style: const TextStyle(fontSize: 30),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(nameOfScreen),
+              backgroundColor: Colors.black,
             ),
-          ),
-          separatorBuilder: (_, __) => Divider(
-            color: Colors.grey,
-            height: heightOption,
-            thickness: thicknessOption,
-          ),
-        ),
-      ),
-    );
+            body: ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              itemCount: _breeds.length,
+              itemBuilder: (_, index) => Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      _breeds[index].breed,
+                      style: const TextStyle(fontSize: 30),
+                    ),
+
+                  ],
+                ),
+              ),
+              separatorBuilder: (_, __) => Divider(
+                color: Colors.grey,
+                height: heightOption,
+                thickness: thicknessOption,
+              ),
+            )));
   }
 }
