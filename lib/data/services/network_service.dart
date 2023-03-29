e064499e67ae81fa82e700b76f07940a9db9106d
Benik <galstyan.benik@gmail.com>
2023-03-29 15:28:04 +0300

    deleted

--- lib/data/services/network_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mocked_list_of_breeds/data/model/breed.dart';
-
-abstract class NetworkService {
-  Future<List<String>> getImage(Breed breed);
-
-  Future<Map<String, dynamic>> getData();
-}
