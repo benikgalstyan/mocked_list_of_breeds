0f217512230d6f2be84fefc34986b1008f3ed530
Benik <galstyan.benik@gmail.com>
2023-03-29 15:35:05 +0300

    implemented runApp for Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mocked_list_of_breeds/presentation/breed_screen.dart';
-import 'package:mocked_list_of_breeds/setup_service_locator.dart';
+import 'package:mocked_list_of_breeds/provider/provider_impl.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  setupServiceLocator();
-  runApp(const BreedScreen());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DataFromApi()),
+      ],
+      child: const BreedScreen(),
+    ),
+  );
 }
