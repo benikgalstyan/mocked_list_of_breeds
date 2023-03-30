eaf4b6e5d1783cc21ed39be76edc94efd8e240be
Benik <galstyan.benik@gmail.com>
2023-03-30 15:17:56 +0300

    Cubit method initialization

--- lib/presentation/image_screen/breed_image_screen.dart
+++ lib/presentation/image_screen/breed_image_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mocked_list_of_breeds/business_logic/bloc/breed_image_bloc.dart';
+import 'package:mocked_list_of_breeds/business_logic/cubit/breed_image_cubit.dart';
 import 'package:mocked_list_of_breeds/data/model/breed.dart';
-import 'package:mocked_list_of_breeds/data/repository/repository.dart';
-import 'package:mocked_list_of_breeds/setup_service_locator.dart';
+import 'package:mocked_list_of_breeds/data/repository/repository_impl.dart';
+import 'package:mocked_list_of_breeds/data/services/network_service_impl.dart';
 import 'breed_image_layout.dart';
 
 class BreedScreenImg extends StatelessWidget {
@@ -13,10 +13,12 @@ class BreedScreenImg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => DogImageBloc(getIt<Repository>()),
-      child: BreedLayoutImg(
-        breed: breed,
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => DogImageCubit(breed,
+            repository: RepositoryImpl(NetworkServiceImpl()))
+          ..fetchImageApi(),
+        child: BreedLayoutImg(breed: breed),
       ),
     );
   }
