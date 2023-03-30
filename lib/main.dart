215c79b6544399bede7e9438f31cefabf17623cc
Benik <galstyan.benik@gmail.com>
2023-03-30 15:20:03 +0300

    Implemented runApp for Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mocked_list_of_breeds/data/repository/repository_impl.dart';
+import 'package:mocked_list_of_breeds/data/services/network_service_impl.dart';
 import 'package:mocked_list_of_breeds/presentation/breed_screen.dart';
-import 'package:mocked_list_of_breeds/setup_service_locator.dart';
 
 void main() {
-  setupServiceLocator();
-  runApp(const BreedScreen());
+  runApp(BreedScreen(
+    repository: RepositoryImpl(NetworkServiceImpl()),
+  ));
 }
