91ccc4c81ef2a7c4d082f1ca75b7279d04583f7f
Benik <galstyan.benik@gmail.com>
2023-04-12 17:23:59 +0300

    deleted async

--- lib/business_logic/riverpod/data_provider.dart
+++ lib/business_logic/riverpod/data_provider.dart
@@ -2,12 +2,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mocked_list_of_breeds/data/model/breed.dart';
 import 'package:mocked_list_of_breeds/data/repository/repository_impl.dart';
 
-final breedsDataProvider = FutureProvider<List<Breed>>((ref) async {
+final breedsDataProvider = FutureProvider<List<Breed>>((ref)  {
   return ref.watch(dataProvider).getData();
 });
 
 final breedsImageProvider =
-    FutureProvider.family<List<String>, Breed>((ref, breed) async {
+    FutureProvider.family<List<String>, Breed>((ref, breed)  {
   final repository = ref.read(dataProvider);
   return repository.getImage(breed);
 });
